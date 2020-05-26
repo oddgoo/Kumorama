f0d90769a56b85424cca348b6fa0d8d0246d25c7
Cuauhtemoc Moreno <oddgoo@gmail.com>
2020-05-26 19:11:15 +1000

    Basic tilemap generation

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,11 @@ export class Main {
 
     private app!: PIXI.Application;
 
+
+
     constructor() {
         window.onload = (): void => {
             this.startLoadingAssets();
-
         };
     }
 
@@ -41,26 +42,70 @@ export class Main {
         loader.load();
     }
 
+    private baseTexture:PIXI.BaseTexture;
+    private stage:PIXI.Container;
+
     private onAssetsLoaded(): void {
 
         this.createRenderer();
+        this.stage = this.app.stage;
 
-        const stage = this.app.stage;
+        this.baseTexture = PIXI.BaseTexture.from("tiles");
+        this.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
-        const texture = PIXI.Texture.from("tiles");
-        texture.frame = new PIXI.Rectangle(16, 0, 16, 16);
-        const tile = new PIXI.Sprite(texture);
-        tile.scale.set(10, 2);
-        tile.position.set(128,128);
+        this.renderLayer();
+        // const texture = PIXI.Texture.from("tiles");
+        //
+        // texture.frame = new PIXI.Rectangle(16, 0, 16, 16);
+        // const bg = new PIXI.Sprite(texture);
+        // bg.scale.set(10, 2);
+        // bg.position.set(128,128);
 
-        this.addRandomSprite();
-        this.addRandomSprite();
+        //this.addRandomSprite();
+        //this.addRandomSprite();
 
-        stage.addChild(tile);
+        //this.stage.addChild(tile);
 
         //Filter
-        const blurFilter1 = new PIXI.filters.BlurFilter();
-        tile.filters = [blurFilter1];
+        const blurFilter1 = new PIXI.filters.BlurFilter(0.5);
+        this.stage.filters = [blurFilter1];
+
+    }
+
+    private renderLayer(): void {
+
+        const layerData:number[][] = [
+            [0,0,6,0,0,0,6],
+            [6,0,0,0,0,0,0],
+            [0,0,5,4,5,2,4],
+            [1,0,1,1,1,1,1]
+        ];
+
+        let row = 0;
+        for (let i in layerData){
+            let column = 0;
+            for(let r of layerData[i]){
+                this.drawTile(r,column,row);
+                column++;
+            }
+            row++;
+        }
+    }
+
+    private static tileScale=4;
+
+    private drawTile(tileId:number, x:number, y:number): void {
+        console.log("drawing tileId " + tileId);
+
+
+
+        let tileTexture = new PIXI.Texture(this.baseTexture, new PIXI.Rectangle(tileId * 16, 0, 16, 16));
+        //let newTexture = new PIXI.Texture(oldTexture.baseTexture, oldTexture.frame);
+
+        let tile = new PIXI.Sprite(tileTexture);
+        tile.scale.set(Main.tileScale, Main.tileScale);
+        tile.position.set(x*16*Main.tileScale + 100, y*16*Main.tileScale + 100);
+        this.stage.addChild(tile);
     }
 
     private createRenderer(): void {
@@ -78,19 +123,19 @@ export class Main {
     }
 
     public addRandomSprite(): void {
-        const texture = PIXI.Texture.from("tiles");
-        texture.frame = new PIXI.Rectangle(32, 0, 16, 16);
-        const tile = new PIXI.Sprite(texture);
-        tile.scale.set(4, 4);
-        tile.position.set(Math.random() * Main.GAME_WIDTH, Math.random() * Main.GAME_HEIGHT);
-        tile.anchor.set(0.5,0.5);
-        this.app.stage.addChild(tile);
-
-        this.app.ticker.add((delta) => {
-            // rotate the container!
-            // use delta to create frame-independent transform
-            tile.rotation -= 0.01 * delta;
-        });
+        // const texture = PIXI.Texture.from("tiles");
+        // texture.frame = new PIXI.Rectangle(32, 0, 16, 16);
+        // const tile = new PIXI.Sprite(texture);
+        // tile.scale.set(4, 4);
+        // tile.position.set(Math.random() * Main.GAME_WIDTH, Math.random() * Main.GAME_HEIGHT);
+        // tile.anchor.set(0.5,0.5);
+        // this.app.stage.addChild(tile);
+        //
+        // this.app.ticker.add((delta) => {
+        //     // rotate the container!
+        //     // use delta to create frame-independent transform
+        //     tile.rotation -= 0.01 * delta;
+        // });
 
     }
 }
